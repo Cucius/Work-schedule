4c2397193f286153fcbc6762471477ae0deb681e
Cucius <Rylan19@Gmail.com>
2022-08-15 19:29:10 -0600

    Added basic time tracking with moment and currently constructing time blocks in the HTML file

---
+++ script.js
@@ -0,0 +1,4 @@
+var currentDate = moment().format("[Today's Date is:] dddd, MMMM Do YYYY, h:mm:ss a");
+console.log(currentDate);
+var currentHour = moment().format("hA");
+console.log("The current hour is:", currentHour);
